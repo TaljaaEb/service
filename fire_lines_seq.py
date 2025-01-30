c7ac6d6234215f71c87b41d96b7ab2fe8be8af5b
TaljaaEb <138305368+TaljaaEb@users.noreply.github.com>
2025-01-30 09:31:35 +0000

    Add files via upload

---
+++ fire_lines_seq.py
@@ -0,0 +1,29 @@
+#fireing sequence
+
+
+import json
+import os
+os.chdir("C:\\Users\\TaljaaEb\\Desktop\\service")
+
+sequence = []
+sequence.append("1") #get_tile
+sequence.append("2") #get_lines
+sequence.append("3") #ret_reply
+sequence.append("4") #send_data
+
+def write_get_reply():
+    # Data to be written
+    dictionary = {
+        "error_code": "",
+        "status": "SUCCESS",
+        "message": "L"
+    }
+ 
+    # Serializing json
+    json_object = json.dumps(dictionary, indent=4)
+    
+    # Writing to sample.json
+    with open("line_events.json", "w") as outfile:
+        outfile.write(json_object)
+
+write_get_reply()
